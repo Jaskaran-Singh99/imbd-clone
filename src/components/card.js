fdfb20444bb74af8c33a7b3d293bbae26e72f2f2
Jaskaran Singh <jaskaran7087065314@gmail.com>
2023-12-13 16:55:46 -0500

    initial commit

---
+++ src/components/card.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import { useState } from 'react'
+import '../styles/card.css'
+import Skeleton, { SkeletonTheme } from 'react-loading-skeleton'
+import "react-loading-skeleton/dist/skeleton.css";
+import { CiStar } from "react-icons/ci";
+import { TbStarFilled } from "react-icons/tb";
+import { FaPlus } from "react-icons/fa6";
+import { useEffect } from 'react'
+
+export const Card = ({ movie }) => {
+
+    const [loading, setLoading] = useState(true)
+    useEffect(() => {
+        setTimeout(() => {
+            setLoading(false)
+        }, 1500)
+    }, [])
+
+    return (
+        <>
+            {
+                loading ?
+                    <div className='card'>
+                        <SkeletonTheme baseColor='#111' highlightColor='#444' >
+                            <Skeleton duration={2} height={300}></Skeleton>
+                        </SkeletonTheme>
+                    </div>
+                    :
+                    <div className='card'>
+                        <div className='image-container'>
+                            <img src={`https://image.tmdb.org/t/p/original${movie.poster_path}`} className='card__img'></img>
+                        </div>
+                        <div className='text-content'>
+                            <div className='review-container'>
+                            <div className='review'>    
+                                <TbStarFilled></TbStarFilled>
+                                <span>{movie.vote_average.toFixed(1)}</span>
+                            </div>
+                            <div className='favorite'>
+                                <CiStar></CiStar>
+                            </div>
+                            </div>
+                            <div className='card__title'>
+                                {movie.title.length > 16 ? <div>{movie.title.substring(0,16)}...</div> : movie.title }
+                            </div>
+                            <button className='watch-now-btn'><span><FaPlus></FaPlus></span>Watchlist</button>
+
+                        </div>
+                    </div>
+
+
+
+            }
+        </>
+    )
+}
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,15 +1,57 @@
 import React from 'react'
 import { useState } from 'react'
 import { useEffect } from 'react'
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+import {Link} from 'react-router-dom'
+import '../styles/home.css'
+import '../styles/card.css'
+import { Card } from '../components/card.js';
 
 export const Home = () => {
-  const [popularMovies, setPopularMovies] = useState('')
+  const [popularMovies, setPopularMovies] = useState([])
   useEffect(()=>{
     fetch('https://api.themoviedb.org/3/movie/popular?api_key=62d64696657178dbaeb82176e7e3e46a').then((res)=>{
-      res.json().then(data=>console.log(setPopularMovies(data.results)))
+      res.json().then(data=>setPopularMovies(data.results))
     })
   },[])
   return (
-    <div>H</div>
+    <>
+    <div className='poster'>
+      <Carousel autoPlay={true}  showStatus={false} showThumbs={false}  >
+        {
+          popularMovies.map(movie=>(
+            <Link style={{textDecoration:"none",color:"white"}} to={`/movie/${movie.id}` } key={movie.id} >
+            <div className="posterImage" >
+                <img src={`https://image.tmdb.org/t/p/original${movie && movie.backdrop_path}`}  />
+            </div>
+            {
+            <div className="posterImage__overlay">
+                <div className="posterImage__title">{movie ? movie.original_title: ""}</div>
+                <div className="posterImage__runtime">
+                    {movie ? movie.release_date : ""}
+                    <span className="posterImage__rating">
+                        {movie ? movie.vote_average :""}
+                        <i className="fas fa-star" />{" "}
+                    </span>
+                </div>
+                <div className="posterImage__description">{movie ? movie.overview : ""}</div>
+            </div> }
+         </Link>
+          ))
+        }
+      </Carousel>
+    </div>
+    
+    <div className='card-container'>
+    {
+      popularMovies.map(movie=>{
+      return <Card movie={movie}></Card>
+      })   
+    }
+    </div>
+    
+    </>
+   
   )
 }
